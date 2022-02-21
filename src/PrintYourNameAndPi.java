4da0d1548bb85014e1ec6225dc7d3f1e353ba1ef
andraxe <99595235+andraxe@users.noreply.github.com>
2022-02-21 14:36:34 +0100

    Initial commit

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,17 @@
+
+import java.lang.Math;
+
+
+
+public class PrintYourNameAndPi {
+
+    public static void main(String[] args) {
+
+        //Print print=new Print();
+
+        System.out.printf(" ANDREA %n " + "The number pi is: " + (Math.PI) );
+
+
+    }
+
+}
